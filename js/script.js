13454077cef6359306b4f7c6e527ceb37c0d6ef5
moonbe77 <moonbe77@gmail.com>
2017-11-12 20:35:10 -0300

    avanzado bastante, funcionando el boton de menu mobil y el slide del menu al hacer scroll

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
-
-
+document.addEventListener('DOMContentLoaded', function () {
   
-  document.addEventListener('DOMContentLoaded', function () {
-    
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length == 0) {
-      console.log("spy menu")
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-          
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-          
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');          
-          
-        });
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    console.log("spy menu")
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');          
+        
       });
-    }
+    });
+  }
+})
+//oculta nav al hacer scroll
+$(document).ready(function (e) {
+  var scroll = 0
+  $(document).scroll(function (e) {    
+    var scrollTop =$(this).scrollTop()
+      if( scrollTop > scroll ){
+        $('.is-fixed-top').css('transform', 'translateY(-124px)')
+        scroll = scrollTop 
+      }
+      if(scrollTop < scroll){
+        $('.is-fixed-top').css('transform', 'translateY(0px)')     
+        scroll = scrollTop        
+      }
+    //console.log(`sTop${$(this).scrollTop()}`)
+    //console.log(`scroll:${scroll}`)
+    
   })
+})
 
 //cargado del menu desde el archivo menu.html
-     /*$(document).ready(function () {
+   /* $(document).ready(function () {
      $('.loadMenu').hide()
       $.get("menu.html", function (data, textStatus, jqXHR) {
           $('.loadMenu').html(data); // dom element
@@ -36,8 +51,8 @@
         "html"
       );    
       $('header').fadeIn('slow')
-      
 });*/
+
 //slider
 var slideIndex = 1;
 showDivs(slideIndex);
